f0df335720edb7e4b83b60052fa0dd06588491af
sartikmj <sartikmj@gmail.com>
2025-06-27 23:56:46 +0530

    vite tailwind and ShadCN UI setup completed !

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Button className="bg-red-600">Check</Button>
+    </>
+  )
+}
+
+export default App
